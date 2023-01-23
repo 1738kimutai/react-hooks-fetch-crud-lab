be436f63af701da2bcaa60c7493b28a5524fc8bc
1738kimutai <davieskimutai4@gmail>
2023-01-23 13:01:32 +0300

    done

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { fetch } from "whatwg-fetch";
 import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
-  const [questions, setQuestions] = React.useState([]);
+  const [questions, setQuestions] = useState([]);
+
+  const FetchFn = () => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((data) =>
+        setQuestions(
+          data.map((q) => {
+            return (
+              <QuestionItem
+                key={q.id}
+                handleChange={handleChange}
+                question={q}
+                deleted={deleted}
+              />
+            );
+          })
+        )
+      );
+  };
+
+  useEffect(() => {
+    FetchFn();
+  }, []);
+
   function deleted(id) {
     fetch(`http://localhost:4000/questions/${id}`, { method: "DELETE" })
-      .then(() => fetcher())
+    .then(
+      () => FetchFn()
+    );
   }
-  function handleChange(e,id) {
-    // console.log(e.target.value+" : question "+id);
+
+  function handleChange(e, id) {
     fetch(`http://localhost:4000/questions/${id}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        "correctIndex": parseInt(e.target.value)
-      })
-    })
+        correctIndex: parseInt(e.target.value),
+      }),
+    });
   }
-  function fetcher() {
-    fetch("http://localhost:4000/questions")
-      .then(resp => resp.json())
-      .then(result => {
-        setQuestions(result.map(que => 
-        <QuestionItem key={que.id} 
-        handleChange={handleChange}
-        question={que} 
-        deleted={deleted} />))
-      })
-  }
-  React.useEffect(() => {fetcher()}, []);
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}
+      <ul>
+        {/* display QuestionItem components here after fetching */}
         {questions}
       </ul>
     </section>
